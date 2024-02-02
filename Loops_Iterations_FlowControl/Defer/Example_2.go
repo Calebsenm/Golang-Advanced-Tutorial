11a451912a22f40377a3069845bf0368bd437759
calebsenm <calebsenm@gmail.com>
2024-02-02 00:36:48 -0500

    OK

---
+++ Loops_Iterations_FlowControl/Defer/Example_2.go
@@ -0,0 +1,19 @@
+
+// Custom Function in defer
+
+package main 
+
+import (
+	"fmt"
+)
+
+func main(){
+
+	defer test();
+	fmt.Println("Executed in main");
+}
+
+
+func test(){
+	fmt.Println("In Defer ");
+}
