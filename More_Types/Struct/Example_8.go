15ecc3670c18094f36eb56740af58c999fa80ca6
calebsenm <calebsenm@gmail.com>
2024-02-05 23:56:22 -0500

    structs

---
+++ More_Types/Struct/Example_8.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+)
+
+type employee struct {
+    Name   string `json:"n"`
+    Age    int    `json:"a"`
+    Salary int    `json:"s"`
+}
+
+func main() {
+    emp := employee{Name: "Sam", Age: 31, Salary: 2000}
+    //Converting to jsonn
+    empJSON, err := json.MarshalIndent(emp, '', '  ')
+    if err != nil {
+        log.Fatalf(err.Error())
+    }
+    fmt.Println(string(empJSON))
+}
