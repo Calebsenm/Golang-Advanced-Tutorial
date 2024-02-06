e6dc624e04556fbb182f46fc89e341ac1638dfb4
Calebsenm <calebgamercraft@gmail.com>
2024-02-06 00:02:45 -0500

    Structs

--- More_Types/Struct/Example_8.go
+++ More_Types/Struct/Example_8.go
@@ -6,16 +6,16 @@ import (
     "log"
 )
 
-type employee struct {
+type Employee struct {
     Name   string `json:"n"`
     Age    int    `json:"a"`
     Salary int    `json:"s"`
 }
 
 func main() {
-    emp := employee{Name: "Sam", Age: 31, Salary: 2000}
+    emp := Employee{Name: "Sam", Age: 31, Salary: 2000}
     //Converting to jsonn
-    empJSON, err := json.MarshalIndent(emp, '', '  ')
+    empJSON, err := json.MarshalIndent(emp, " ",  "  ")
     if err != nil {
         log.Fatalf(err.Error())
     }
