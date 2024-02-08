f338e9179e3960bf97714b8e7185895d653abef5
calebsenm <calebsenm@gmail.com>
2024-02-07 23:49:42 -0500

    Arrays

---
+++ More_Types/Array/Example_3.go
@@ -0,0 +1,18 @@
+// Accesing array elements
+
+package main  
+
+import "fmt"
+
+func main(){
+
+	sample := [2] string{ "aa", "bb"}
+
+	
+    fmt.Println(sample[0])
+    fmt.Println(sample[1])
+
+	sample[0] = "xx"
+	fmt.Println(sample)
+
+}
