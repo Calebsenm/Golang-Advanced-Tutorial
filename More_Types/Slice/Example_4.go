7d9e475cfaa3c75f5d9b13d452beef3bc553f60f
Calebsenm <calebgamercraft@gmail.com>
2024-02-08 23:48:54 -0500

    Slices

---
+++ More_Types/Slice/Example_4.go
@@ -0,0 +1,22 @@
+// Package main
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	numbers := make([]int, 3, 5)
+	fmt.Printf("numbers=%v\n", numbers)
+	fmt.Printf("length=%d\n", len(numbers))
+	fmt.Printf("capacity=%d\n", cap(numbers))
+
+	//With capacity ommited
+	numbers = make([]int, 3)
+	fmt.Println("\nCapacity Ommited")
+	fmt.Printf("numbers=%v\n", numbers)
+	fmt.Printf("length=%d\n", len(numbers))
+	fmt.Printf("capacity=%d\n", cap(numbers))
+}
