f655d0dbf9996288dc1dc2608ec044ca6565af1f
Calebsenm <calebgamercraft@gmail.com>
2024-02-09 23:53:37 -0500

    Maps

---
+++ More_Types/Maps/Example_9.go
@@ -0,0 +1,14 @@
+// Zero Value
+
+package main  
+
+import "fmt"
+
+func main(){
+
+	var employeeSalary map[string] int 
+
+	if employeeSalary == nil {
+		fmt.Println("employeeSalary map is nil ");
+	}
+}
