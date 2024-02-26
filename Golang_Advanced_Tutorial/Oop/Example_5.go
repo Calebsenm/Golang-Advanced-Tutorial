b062fc5498ec2f799cc42988669bbc840e10f78b
Calebsenm <calebgamercraft@gmail.com>
2024-02-26 18:48:16 -0500

    OOP

---
+++ Golang_Advanced_Tutorial/Oop/Example_5.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+type iBase interface {
+	say()
+}
+
+type base struct {
+	color string
+	clear func()
+}
+
+func (b *base) say() {
+	b.clear()
+}
+
+type child struct {
+	base  //embeding
+	style string
+}
+
+func check(b iBase) {
+	b.say()
+}
+
+func main() {
+
+	base := base{color: "Red",
+        clear: func() {
+            fmt.Println("Clear from child's function")
+        }}
+
+	child := &child{
+		base: base,
+		style: "Somestyle",
+	}
+	child.say();
+	
+}
