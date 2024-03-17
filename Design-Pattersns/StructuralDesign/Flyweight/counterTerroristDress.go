779eb8d26ac9eaab94e677654d9958f8f96d7c34
calebsenm <calebsenm@gmail.com>
2024-03-17 01:36:51 -0500

    OK

---
+++ Design-Pattersns/StructuralDesign/Flyweight/counterTerroristDress.go
@@ -0,0 +1,14 @@
+
+package main
+
+type counterTerroristDress struct {
+    color string
+}
+
+func (c *counterTerroristDress) getColor() string {
+    return c.color
+}
+
+func newCounterTerroristDress() *counterTerroristDress {
+    return &counterTerroristDress{color: "green"}
+}
