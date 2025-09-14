1b65203438c3cb27e2b46104c7efb081dd861c3e
calebsenm <calebsenm@gmail.com>
2025-09-13 20:33:33 -0500

    decorator

--- Design-Pattersns/StructuralDesign/Decorator/main.go
+++ Design-Pattersns/StructuralDesign/Decorator/main.go
@@ -1,65 +1,22 @@
+
 package main
 
 import "fmt"
 
-type pizza interface {
-	getPrice() int
-}
-
-type peppyPaneer struct {
-}
-
-func (p *peppyPaneer) getPrice() int {
-	return 20
-}
-
-type veggeMania struct {
-}
-
-func (p *veggeMania) getPrice() int {
-	return 15
-}
-
-type tomatoTopping struct {
-	pizza pizza
-}
-
-func (c *tomatoTopping) getPrice() int {
-	pizzaPrice := c.pizza.getPrice()
-	return pizzaPrice + 7
-}
-
-type cheeseTopping struct {
-	pizza pizza
-}
-
-func (c *cheeseTopping) getPrice() int {
-	pizzaPrice := c.pizza.getPrice()
-	return pizzaPrice + 10
-}
-
 func main() {
 
-	veggiePizza := &veggeMania{}
+    pizza := &VeggieMania{}
 
-	//Add cheese topping
-	veggiePizzaWithCheese := &cheeseTopping{
-		pizza: veggiePizza,
-	}
+    //Add cheese topping
+    pizzaWithCheese := &CheeseTopping{
+        pizza: pizza,
+    }
 
-	//Add tomato topping
-	veggiePizzaWithCheeseAndTomato := &tomatoTopping{
-		pizza: veggiePizzaWithCheese,
-	}
+    //Add tomato topping
+    pizzaWithCheeseAndTomato := &TomatoTopping{
+        pizza: pizzaWithCheese,
+    }
 
-	fmt.Printf("Price of veggieMania pizza with tomato and cheese topping is %d\n", veggiePizzaWithCheeseAndTomato.getPrice())
-
-	peppyPaneerPizza := &peppyPaneer{}
-
-	//Add cheese topping
-	peppyPaneerPizzaWithCheese := &cheeseTopping{
-		pizza: peppyPaneerPizza,
-	}
-
-	fmt.Printf("Price of peppyPaneer with tomato and cheese topping is %d\n", peppyPaneerPizzaWithCheese.getPrice())
+    fmt.Printf("Price of veggeMania with tomato and cheese topping is %d\n", pizzaWithCheeseAndTomato.getPrice())
 }
+
